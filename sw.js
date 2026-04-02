8d72056c4cbad46d5d1f6eb882895e11f2c157c6
QPC2026 <qpcultra@gmail.com>
2026-04-02 13:07:33 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'diamond-core-v1';
+const assets = ['./', './index.html'];
+
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// Escuchar peticiones
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
